af40d62c568a59b948982ba5cda995ae7f95c25c
ndimangwa <ndimangwa@gmail.com>
2026-02-01 14:39:30 +0300

    Work on Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const Homepage = () => {
+    //console.log(process.env.MONGODB_URI);
     return ( 
     <>
         <Hero/>
         <InfoBoxes/>
+        <HomeProperties/>
     </> 
     );
 }
